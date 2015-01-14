89d1bce9acb64d9fce666df7df9dfeb564257e7b
Adam Anderson <horatio.blackwood@gmail.com>
2015-01-13 20:53:34 -0800

    Added the InputManager interface.

---
+++ src/pongline/display/InputManager.java
@@ -0,0 +1,9 @@
+package pongline.display;
+
+/**
+ * A class that accepts user input and makes it available to the GameProcessor when its requested.
+ * @author adam
+ */
+public interface InputManager {
+
+}
