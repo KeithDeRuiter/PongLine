42e7cb6014951d1dd846506c5a7b01a637c158dd
Adam Anderson <horatio.blackwood@gmail.com>
2015-01-24 20:55:03 -0800

    Updated Pongline javadoc.

--- src/pongline/PongLine.java
+++ src/pongline/PongLine.java
@@ -1,22 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package pongline;
 
 /**
  *
- * @author Keith
+ * @author Adam
  */
 public class PongLine {
 
     /**
+     * The main class of this application.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
