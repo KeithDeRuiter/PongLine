b5ca2c0cae4f75a8148820c44c08bacd902bd626
Terra <Terra@CaptainTerra>
2015-01-30 12:28:12 -0800

    Added Assets

---
+++ src/pongline/display/Asset.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pongline.display;
+
+/**
+ *
+ * @author LoTB
+ */
+public enum Asset {
+    BALL,
+    COUNTDOWN_SOUND,
+    PADDLE,
+    PADDLE_SOUND,
+    SCORE_SOUND,
+    START_SOUND,
+    WALL_SOUND,
+    WIN_SOUND,
+}
