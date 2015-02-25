3b5aa6f2cb80abd7597d21e82e39dd95ffb1dea9
Adam Anderson <horatio.blackwood@gmail.com>
2015-02-24 21:42:38 -0800

    Added Setup Display and Listener for game init.  Added Limited Length Integer Document Class.  Removed private files from repository.

--- src/pongline/PongLine.java
+++ src/pongline/PongLine.java
@@ -1,17 +1,21 @@
 package pongline;
 
+import pongline.display.SetupDisplay;
+
 /**
  *
  * @author Adam
  */
 public class PongLine {
 
+    /** The version number of this build. */
+    public static final String VERSION = "v0.1";
+
     /**
      * The main class of this application.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
 
 }
