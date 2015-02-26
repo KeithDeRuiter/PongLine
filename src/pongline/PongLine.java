d38f780771aad90808744809ab4e454bc00a6162
Adam Anderson <horatio.blackwood@gmail.com>
2015-02-25 23:32:46 -0800

    Laid the foundation for application initialization.

--- src/pongline/PongLine.java
+++ src/pongline/PongLine.java
@@ -1,6 +1,8 @@
 package pongline;
 
 import pongline.display.SetupDisplay;
+import pongline.display.SetupListener;
+import pongline.input.InputManager;
 
 /**
  *
@@ -11,11 +13,13 @@ public class PongLine {
     /** The version number of this build. */
     public static final String VERSION = "v0.1";
 
+
     /**
      * The main class of this application.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        GameInitializer gi = new GameInitializer();
+        gi.startClient();
     }
-
 }
